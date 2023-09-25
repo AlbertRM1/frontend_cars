3e15c1d7aa53c45adc76f2f8f9a2dde152f95700
Albert Romero Martinez <132682989+AlbertRM1@users.noreply.github.com>
2023-09-25 22:47:34 +0200

    Add files via upload

--- src/Pages/Models.jsx
+++ src/Pages/Models.jsx
@@ -2,7 +2,13 @@ import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
-/* import CarImg1 from "../images/cars-big/audi-box.png"; */
+import A1Image from "../images/cars-big/a1.jpg";
+import BMW320Image from "../images/cars-big/320.jpg";
+import BenzImage from "../images/cars-big/benz.jpg";
+import Golf6Image from "../images/cars-big/golf6.jpg";
+import PassatImage from "../images/cars-big/passat.jpg";
+import CamryImage from "../images/cars-big/camry.jpg";
+import DefaultImage from "../images/cars-big/defaultimage.jpg";
 
 function Models() {
   const [cars, setCars] = useState([]);
@@ -20,6 +26,27 @@ function Models() {
       .catch((error) => console.error("Error al obtener cars:", error));
   }, []); // El [] vacío asegura que esta solicitud se realice solo una vez al cargar el componente
 
+  const getModelImage = (model) => {
+    // Mapear el modelo al nombre de archivo de imagen correspondiente
+    switch (model.toLowerCase()) {
+      case "a1":
+        return A1Image;
+      case "320":
+        return BMW320Image;
+      case "benz":
+        return BenzImage;
+      case "golf6":
+        return Golf6Image;
+      case "passat":
+        return PassatImage;
+      case "camry":
+        return CamryImage;
+      default:
+        // Si el modelo no coincide con ninguno de los anteriores, puedes devolver una imagen predeterminada o mostrar un mensaje de error.
+        return DefaultImage;
+    }
+  };
+
   return (
     <>
       <Navbar />
@@ -30,7 +57,7 @@ function Models() {
             {cars.map((car, index) => (
               <div className="models-div__box" key={index}>
                 <div className="models-div__box__img">
-                  {/* <img src={CarImg1} alt="car_img" /> */}
+                <img src={getModelImage(car.model)} alt={`${car.make} ${car.model}`} />
                 </div>
                 <div className="models-div__box__descr">
                   <div className="models-div__box__descr__name-price">
